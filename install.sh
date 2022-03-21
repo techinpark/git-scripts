f26bd21a19e7a5de36bf341ccfc26c85c53c9975
fernando <fernando@kakao.com>
2022-03-21 15:54:28 +0900

    🎉 Initial Commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# copy git script files 
+EXPORT_PATH='export PATH="$HOME/.gitsh:$PATH"'
+cp -R .gitsh ~/.gitsh 
+echo $EXPORT_PATH >> ~/.zshrc
+echo " git scripts have been set up. enjoy 🎉"
